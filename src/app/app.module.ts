9ff98311bdce9c9997a93a09afe0a3e6b0b547d3
jbs\yendo <yoshikazu.endo@jbs.com>
2017-12-11 15:56:04 +0900

    入門06を実施した。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 // AppComponentコンポーネントをインポート
 // app.component.tsをインポート
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroinesComponent } from './heroines.component'
 import { HeroineDetailComponent } from './heroine-detail.component';
 import { HeroineService } from './-share/services/heroine.service';
@@ -16,14 +18,36 @@ import { HeroineService } from './-share/services/heroine.service';
   // 使用するコンポーネントクラスを記述する。
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroinesComponent,
     HeroineDetailComponent
   ],
   // アプリケーションをブラウザで動かすために用いられる（おまじない）
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        // パスの指定がない場合のリダイレクト先を指定（ここではdashboardを指定）
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        // ダッシュボード用のRouterモジュール
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        // forRootメソッド：pathプロパティにURLのパス、componentプロパティにコンポーネント名を定義すると、
+        // 画面を遷移した時に、ブラウザのアドレスバーにこのパスが加わる。
+        // テンプレートで<router-outlet></router-outlet>を指定すると、リンクしたパスのコンポーネントがそこに差し込まれる。
+        path: 'heroines',
+        component: HeroinesComponent
+      }
+    ])
   ],
+  // providersに指定すると、子のコンポーネントがサービスの同じインスタンスを使える！
   providers: [ HeroineService ],
   bootstrap: [ AppComponent ]
 })
---
+++ src/app/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Heroine } from './-share/services/heroine';
+import { HeroineService } from './-share/services/heroine.service';
+@Component({
+    selector: 'my-dashboard',
+    templateUrl: './dashboard.component.html'
+})
+export class DashboardComponent implements OnInit {
+    heroines: Heroine[] = [];
+    constructor(private heroineService: HeroineService) {}
+    ngOnInit(): void{
+        this.heroineService.getHeroines()
+        .then((heroines: Heroine[]) => this.heroines = heroines.slice(0, 4));
+    }
+}
